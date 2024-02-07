5b2767a3c79b6d13d6d2a76d1a880205e9c04b02
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-02-08 01:18:47 +0300

    implement user service

---
+++ Auth.Service/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Auth.Core.Entities;
+using Auth.Core.Service;
+using AutoMapper;
+using AutoMapper.Internal.Mappers;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.DTO;
+
+namespace Auth.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+
+
+        public UserService(UserManager<User> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseDTO<UserDTO>> CreateUserAsync(CreateUserDTO userDTO)
+        {
+            var user = new User { Email = userDTO.EMail, UserName = userDTO.UserName };
+            var result = await _userManager.CreateAsync(user, userDTO.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description).ToList();
+                return ResponseDTO<UserDTO>.Fail(400, new ErrorDTO(errors, true));
+            }
+            return ResponseDTO<UserDTO>.Succes(_mapper.Map<UserDTO>(user), 200);
+        }
+
+        public async Task<ResponseDTO<UserDTO>> GetUserByName(string name)
+        {
+            var user=await _userManager.FindByNameAsync(name);
+            if (user == null) ResponseDTO<UserDTO>.Fail(404, "User Not FOund",true);
+            return ResponseDTO<UserDTO>.Succes(_mapper.Map<UserDTO>(user), 200);
+        }
+    }
+}
